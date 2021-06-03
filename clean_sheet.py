6f95ed4c56e6056ea8e1535be2f9d7306444e7dd
Don George <72986754+Don-George-Thayyil@users.noreply.github.com>
2021-06-03 13:10:05 +0530

    Add files via upload
    give as many lists of data as you want. It prints out a clean table.

---
+++ clean_sheet.py
@@ -0,0 +1,48 @@
+def clean_sheet(*kwargs):
+    final_array = []
+    number = len(kwargs[0])
+    arg_length = len(kwargs)
+    kwargs[0].sort()
+    for item in kwargs:
+        array = []
+        longest = item[0]
+        length = len(item[0])
+        i = 0 
+        j = 1
+        while j < len(item):
+            if len(item[j]) > len(item[i]):
+                length = len(item[j])
+            i += 1
+            j += 1
+        print("item = ",item)
+        for elements in item:
+            if len(elements) < length+1:
+                add = (length+1) - len(elements) 
+                elements = elements+" "*add
+            array.append(elements)
+        final_array.append(array)
+    for i in range(number):
+        j = 0
+        while j < arg_length:
+            print(final_array[j][i], end = " ")
+            j += 1
+        print()
+
+
+if __name__ == "__main__": 
+    first_names = ["Adhityan","Adwaith","Arjun","Arun","Athmaja","Athul","Bobby","Don"]
+    last_names = ["M","S Kumar","VK","K Paul","M Nair","PV","Shaju","George"]
+    marks = ["43","38","48","39","49","45","40","40"]
+    clean_sheet(first_names,last_names,marks)
+
+
+    
+
+
+
+
+
+# full_name = first_names[i]+" "+last_names[i] 
+# file.write(row)
+# file.write("\n")
+# file.close()
